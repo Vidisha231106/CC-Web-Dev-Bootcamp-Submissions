46dd9bcc2b564fd2ffa52508d909790b68625cd8
Karthikeya47 <karthikeyaneelagiri@gmail.com>
2026-03-22 23:35:45 +0530

    final

--- NeelagiriKarthikeya_1RV25CS111/Assignment_1/script.js
+++ NeelagiriKarthikeya_1RV25CS111/Assignment_1/script.js
@@ -1,20 +1,30 @@
-function addItem(text,quer) {
-       const li = document.createElement('li');
-       li.textContent = text;
-       document.querySelector(quer).appendChild('li');
-    }
-
+const tasks=[];
+let task_n=0;
 document.getElementById('addtaskbtn').addEventListener('click',()=>{
     const task_ = (document.getElementById('addtask').value);
-    //console.log(tasks)
     const card = document.createElement('li');
-    const clrbtn=document.createElement('button');
-    clrbtn.classList.add('clrbtn')
-    card.classList.add('card');
-    clrbtn.textContent='done';
-    card.textContent = task_;
-    document.querySelector('#tasks').appendChild(card);
-    document.querySelector('#tasks').appendChild(clrbtn);
-    //addItem(task_,'#card');
     
+    const text_=document.createElement('span');
+    text_.textContent=task_;
+    text_.classList.add('task-text');
+
+    const clrbtn=document.createElement('button');
+    clrbtn.classList.add('clrbtn');
+    clrbtn.textContent='Done';
+
+
+    card.classList.add('card');
+
+    clrbtn.id=task_n;
+    
+    card.appendChild(text_);
+    card.appendChild(clrbtn);
+    document.querySelector('#eachtask').appendChild(card);
+    tasks.push(task_);
+    document.getElementById(task_n).addEventListener('click',()=>{
+    card.remove();
+    });
+    task_n++;
+    document.getElementById('addtask').value = "";
 });
+
